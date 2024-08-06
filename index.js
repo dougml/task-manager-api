dc6eb5f6456a58563e4469ac6c153de5dbdf5f88
Douglas Mota <106937525+dougml@users.noreply.github.com>
2024-08-06 16:59:56 -0300

    Firts commit

---
+++ index.js
@@ -0,0 +1,19 @@
+const express = require("express");
+require("dotenv").config();
+
+const app = express();
+
+const PORT = process.env.PORT;
+const connectToDatabase = require("./src/db/conn");
+connectToDatabase();
+
+app.get("/", (req, res) => {
+    res.status(200).send({
+        message: "Olá",
+        secondMessage: "Mundo",
+    });
+});
+
+app.listen(PORT, () => {
+    console.log(`Server is running in port: ${PORT}`);
+});
