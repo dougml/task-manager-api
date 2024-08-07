b1bc7904c3cc186b1cf2da26c62409fec7e6be64
Douglas Mota <106937525+dougml@users.noreply.github.com>
2024-08-07 13:59:57 -0300

    feat: added cors on app

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const express = require("express");
 require("dotenv").config();
 const TaskRouter = require('./src/routes/task.routes')
+const cors = require('cors')
 
 const app = express();
+app.use(cors())
 app.use(express.json());
 
 const PORT = process.env.PORT;
